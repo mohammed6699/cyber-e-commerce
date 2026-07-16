42fb71b7dc3659bbd66d7e0815ce474f58f05d31
mohammed6699 <mohammedahmed.refaat@gmail.com>
2026-07-16 09:40:52 +0300

    adding my first commit for project structure files

---
+++ src/app/app.config.ts
@@ -0,0 +1,22 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import {provideTranslateService} from "@ngx-translate/core";
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'en',
+      lang: 'en'
+    })
+  ]
+};
