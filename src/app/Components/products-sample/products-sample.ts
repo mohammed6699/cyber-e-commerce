59b1c29ec180ba1c5b2ca1d793fecdb7bdee4cb8
mohammed6699 <mohammedahmed.refaat@gmail.com>
2026-07-16 09:45:34 +0300

    handle the used component for the project navbar, hero, login, popular products and sample products

---
+++ src/app/Components/products-sample/products-sample.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-sample',
+  imports: [],
+  templateUrl: './products-sample.html',
+  styleUrl: './products-sample.css',
+})
+export class ProductsSample {}
