2d531cd3562cc7a3cdfed1a37a5d3c2b6bd80dc7
mohammed6699 <mohammedahmed.refaat@gmail.com>
2026-07-16 09:47:48 +0300

    handle the shared components for the project

---
+++ src/app/Shared/product-card/product-card.ts
@@ -0,0 +1,43 @@
+import { Component, computed, EventEmitter, Input, Output } from '@angular/core';
+import { ProductModel } from '../../Models/Product.model';
+import { Router } from '@angular/router';
+import { CurrencyPipe, NgStyle, NgClass } from '@angular/common';
+import { WishListService } from '../../Services/WishList.service';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [NgStyle, CurrencyPipe, NgClass],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {
+  @Input({required: true}) product!: ProductModel
+  @Input() showDetailsBtn: boolean = false;
+  @Output() addToCart = new EventEmitter<ProductModel>();
+  @Output() wishList = new EventEmitter<ProductModel>();
+  @Output()navigateToDetails = new EventEmitter<number>();
+  // remeber to use some as it return true or false
+  isWishList = computed(() => this.wishlistSer.items().some((item) => item.id === this.product.id));
+
+  constructor(
+    private router: Router,
+    public wishlistSer: WishListService
+  ){}
+  
+  onWishListClick(): void{
+    this.wishlistSer.toogleWishList(this.product)
+    
+  }
+  onAddToCartClick(): void{
+    if(this.product.stock > 0){
+      this.addToCart.emit(this.product)
+    }
+  }
+  navigateToProductDetails(): void{
+    this.navigateToDetails.emit(this.product.id)
+  }
+  productOriginalrice(price: number, discountPercentage: number){
+    const originalPrice = price - (price * (discountPercentage / 100));
+    return originalPrice.toFixed(2);
+  }
+}
---
+++ src/app/Shared/reviews-component/reviews-component.ts
@@ -0,0 +1,62 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductModel, SearchProducts } from '../../Models/Product.model';
+import { DatePipe, NgStyle } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ProductCard } from "../product-card/product-card";
+import { CartService } from '../../Services/Cart.service';
+import { ProductService } from '../../Services/Product.service';
+
+@Component({
+  selector: 'app-reviews-component',
+  imports: [DatePipe, NgStyle, FormsModule, ProductCard],
+  templateUrl: './reviews-component.html',
+  styleUrl: './reviews-component.css',
+})
+export class ReviewsComponent{
+  @Input({required: true}) product!: ProductModel
+  selectedCategories: string[] = [];
+  products!: ProductModel[];
+  newComment: string = '';
+  relatedProducts: ProductModel[] = [];
+
+  constructor(
+    private cartSer: CartService,
+    private prouctSer: ProductService
+  ){}
+
+
+  submitReview(): void {
+    if (this.newComment.trim()) {
+      this.product.reviews.unshift({
+        reviewerName: 'Current User', 
+        date: new Date().toISOString(),
+        comment: this.newComment,
+        rating: 5,
+        reviewerEmail: 'emailll' 
+      });
+      this.newComment = '';
+    }
+    localStorage.setItem('user review', JSON.stringify(this.product.reviews))
+  }
+
+  AvgRate(): string {
+    if (this.product.reviews.length === 0) return '0.0';
+    const sum = this.product.reviews.reduce((acc, review) => acc + review.rating, 0);
+    return (sum / this.product.reviews.length).toFixed(1);
+  }
+  TotalReviews(){
+    return this.product.reviews.length;
+  }
+  StarsDistributionPercentage(stars: number): number {
+    if (this.product.reviews.length === 0) return 0;
+    const count = this.product.reviews.filter((review) => review.rating === stars).length;
+    return Math.round((count / this.product.reviews.length) * 100);
+  }
+  
+  AddToCart(){
+
+  }
+  AddToWishlist(){
+
+  }
+}
