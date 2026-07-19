74c6db05f258963b84ec65e3d51f5fc24bfd08e7
mohammed6699 <mohammedahmed.refaat@gmail.com>
2026-07-19 15:16:18 +0300

    add the reset of component and rechange files postion

--- src/app/Components/announcement/announcement.ts
+++ src/app/Components/announcement/announcement.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { ProductModel } from '../../Models/Product.model';
 import { RouterLink } from "@angular/router";
+import { NgClass } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-announcement',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass, TranslatePipe],
   templateUrl: './announcement.html',
   styleUrl: './announcement.css',
 })
-export class Announcement {
+export class Announcement{
+  curentLanguage: any;
+ 
   @Input() products: ProductModel[] = [];
 }
--- src/app/Components/discount-products/discount-products.ts
+++ src/app/Components/discount-products/discount-products.ts
@@ -1,23 +1,27 @@
-import { ChangeDetectorRef, Component, Input } from '@angular/core';
-import { ProductService } from '../../Services/Product.service';
+import { Component, Input, OnInit } from '@angular/core';
 import { ProductModel } from '../../Models/Product.model';
 import { ProductCard } from "../../Shared/product-card/product-card";
 import { CartService } from '../../Services/Cart.service';
-import { ProductDetails } from '../../Features/product-details/product-details';
 import { HotToastService } from '@ngxpert/hot-toast';
+import { TranslatePipe } from '@ngx-translate/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-discount-products',
-  imports: [ProductCard],
+  imports: [ProductCard, TranslatePipe,NgClass],
   templateUrl: './discount-products.html',
   styleUrl: './discount-products.css',
 })
-export class DiscountProducts{ 
+export class DiscountProducts implements OnInit{ 
   @Input() products: ProductModel[] = [];
+  currentLang: any;
   constructor(
     private cartSer: CartService,
     private toast: HotToastService
-  ) { }
+  ) {}
+  ngOnInit(): void {
+    this.currentLang = localStorage.getItem('language');
+  }
   addTocart(product: ProductModel){
     this.toast.success(`${product.title} added to cart succefully`);
     this.cartSer.addToCart(product)
