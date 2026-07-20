21cc447ba6d107288b3d56d31744089441ab2871
mohammed6699 <mohammedahmed.refaat@gmail.com>
2026-07-20 10:07:12 +0300

    remove unused import

--- src/app/Shared/card-data/card-data.ts
+++ src/app/Shared/card-data/card-data.ts
@@ -1,10 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ProductModel } from '../../Models/Product.model';
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-card-data',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './card-data.html',
   styleUrl: './card-data.css',
 })
